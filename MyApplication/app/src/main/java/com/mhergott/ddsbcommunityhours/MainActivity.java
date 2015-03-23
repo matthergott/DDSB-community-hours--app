dff3944b1948b26985ed55dcae838c23a107c616
matt <matthewhergott@sympatico.ca>
2015-03-22 20:34:04 -0400

    22/03/15 part 3
    tightened up your additions. Excellent work keep it up.

--- MyApplication/app/src/main/java/com/mhergott/ddsbcommunityhours/MainActivity.java
+++ MyApplication/app/src/main/java/com/mhergott/ddsbcommunityhours/MainActivity.java
@@ -2,6 +2,7 @@ package com.mhergott.ddsbcommunityhours;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -182,17 +183,17 @@ public class MainActivity extends ActionBarActivity implements android.support.v
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()){
+            case R.id.main_add_new_log:
+                Intent i = new Intent(this, AddNewLog.class);
+                startActivity(i);
+                return true;
+
+            default:
+                super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     public void arrCreator(String str, boolean x)
